579a7a99f403372384b758b44e1e0558a1aeb407
Kameron Seto <tan@Sad-Panda.local>
2018-12-03 14:02:46 -0800

    example data)

--- database/seedBase.js
+++ database/seedBase.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const transit = require('./Transit.js');
+const { Line, Direction, Pattern } = require('./Transit.js');
 const config = require('./config.js');
 
 // Line, Direction, Pattern };
@@ -9,13 +9,13 @@ const linesUri = `http://api.511.org/transit/lines?format=json&api_key=${config.
 // the first character is BOM
 // axios.get(linesUri)
 //   .then((res) => {
-//     transit.Line.insertMany(JSON.parse(res.data.slice(1)), (err, docs) => {
+//     Line.insertMany(JSON.parse(res.data.slice(1)), (err, docs) => {
 //       if (err) console.error(err);
 //       console.log(`${docs.length} lines inserted.`);
 //     });
 //   });
 
-transit.Direction.insertMany([
+Direction.insertMany([
   { DirectionId: 'IB', Name: 'Inbound' },
   { DirectionId: 'OB', Name: 'Outbound' },
 ], (err, docs) => {
@@ -23,12 +23,12 @@ transit.Direction.insertMany([
   console.log(`${docs.length} directions inserted.`);
 });
 
-transit.Line.distinct('Id', (err, lineIds) => {
+Line.distinct('Id', (err, lineIds) => {
   if (err) console.error(err);
 
   // limiting //////////////////////////////////////////////
   // lineIds.forEach((line) => {
-  ['43', '44', 'K', 'L', 'M', 'N'].forEach((line) => {
+  ['43', 'L'].forEach((line) => {
     axios.get(`http://api.511.org/transit/patterns?format=json&api_key=${config.API_TOKEN}&operator_id=SF&line_id=${line}`)
       .then((res) => {
         JSON.parse(res.data.slice(1)).journeyPatterns.forEach((pattern) => {
@@ -41,7 +41,7 @@ transit.Line.distinct('Id', (err, lineIds) => {
             StopPointInJourneyPattern: PointsInSequence.StopPointInJourneyPattern,
           };
 
-          transit.Pattern.insertOne(data, (err, docs) => {
+          Pattern.create(data, (err, docs) => {
             if (err) console.error(err);
             console.log('docs: ', docs);
             console.log(`Pattern inserted: ${serviceJourneyPatternRef}, ${LineRef}, ${DirectionRef}`);
@@ -52,7 +52,32 @@ transit.Line.distinct('Id', (err, lineIds) => {
 });
 
 
-  /*
+/*
+
+var data = {
+  serviceJourneyPatternRef: '12345',
+  LineRef: '44',
+  DirectionRef: 'OB',
+  StopPointInJourneyPattern: [
+    {"StopPointInJourneyPatternId":"10579524","Order":"1","ScheduledStopPointRef":"14852","Name":"Lawton St \u0026 7th Ave"},
+    {"StopPointInJourneyPatternId":"10579734","Order":"2","ScheduledStopPointRef":"16933","Name":"Woodside Ave \u0026 Balceta Ave"},
+  ],
+};
+Pattern.create(data, (err, docs) => {
+  if (err) console.error(err);
+  console.log('docs: ', docs);
+  console.log(`Pattern inserted: ${data.serviceJourneyPatternRef}, ${data.LineRef}, ${data.DirectionRef}`);
+});
+
+
+
+
+*/
+
+
+
+
+/*
 
 const detectCharacterEncoding = require('detect-character-encoding');
 
--- database/seedItinerary.js
+++ database/seedItinerary.js
@@ -1,2 +1,14 @@
-const db = require('./db.js');
+const { Itinerary } = require('./Transit.js');
 
+Itinerary.create({
+  name: 'Morning Commute',
+  waypoints: [{
+    id: '14852',
+    name: 'Lawton St \u0026 7th Ave',
+    line: '44',
+    direction: 'OB',
+  }],
+}, (err, docs) => {
+  if (err) console.error(err);
+  console.log('Itinerary created.');
+});
