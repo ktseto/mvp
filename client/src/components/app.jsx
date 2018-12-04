deb521c4fe9e3979ccaf42a6a265284b7d75f3db
Kameron Seto <tan@Sad-Panda.local>
2018-12-03 18:50:20 -0800

    most elements on home page ready; working on edit page

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -8,40 +8,71 @@ class App extends React.Component {
     this.state = {
       itineraries: [],
       lastUpdatedTime: null,
+      onHomePage: true,
     };
+
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   componentDidMount() {
-    axios.get('/realtime')
-      .then((res) => {
-        this.setState({
-          itineraries: res.data,
-        });
-      });
+    // axios.get('/realtime')
+    //   .then((res) => {
+    //     this.setState({
+    //       itineraries: res.data,
+    //     });
+    //   });
   }
 
-  // {this.state.itineraries.map(itin => (<div>'AAAAA'</div>))}
+  handleEdit() {
+    this.setState({
+      onHomePage: false,
+    });
+  }
 
   render() {
-    console.log(this.state.itineraries);
     return (
       <div>
-        <div>Best Transit App Ever</div>
-        <button>Edit Itineraries</button>
-        {this.state.itineraries.map(itin => (
-          <div key={itin['_id']}>
-            <div>Itinerary:  {itin.name}</div>
-            {itin.waypoints.map(wp => (
-              <div key={wp.id}>
-                <div>{wp.line}</div>
-                <div>{wp.direction}</div>
-                <div>{wp.name}</div>
-                <div>{`${wp.arrivalTimes.join(', ')} mins`}</div>
-              </div>
-            ))}
-          </div>
-        ))}
-        <div>Last update: {this.state.lastUpdatedTime}</div>
+      {
+        this.state.onHomePage
+        ?
+        <div id="realTime">
+          <div id="appHeader">Best Transit App Ever</div>
+          <button id="editItin" onClick={this.handleEdit}>Edit Itineraries</button>
+          {this.state.itineraries.map(itin => (
+            <div key={itin._id}>
+              <div>Itinerary:  {itin.name}</div>
+              {itin.waypoints.map(wp => (
+                <div key={wp.id}>
+                  <div>{wp.line}</div>
+                  <div>{wp.direction}</div>
+                  <div>{wp.name}</div>
+                  <div>{`${wp.arrivalTimes.join(', ')} mins`}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+          <div id="lastUpdate">Last update: {this.state.lastUpdatedTime}</div>
+        </div>
+        :
+        <div id="editItin">
+          <div id="editHeader">Edit Itineraries</div>
+          {this.state.itineraries.map(itin => (
+            <div key={itin._id}>
+              <div>Itinerary:  {itin.name}</div>
+              <button id={itin._id}>X</button>
+              {itin.waypoints.map(wp => (
+                <div key={wp.id}>
+                  <div>{wp.line}</div>
+                  <div>{wp.direction}</div>
+                  <div>{wp.name}</div>
+                  <button id={`${itin._id}/${wp.id}`}>X</button>
+                </div>
+              ))}
+            </div>
+          ))}
+          <button>Return</button>
+        </div>
+      }
       </div>
     );
   }
